c8cc463b926889edf58a3fddf35e06ff7fb1126e
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2021-12-14 23:24:26 +0300

    Initial commit

--- src/main/java/ru/geekbrains/jv2lesson2/Main.java
+++ src/main/java/ru/geekbrains/jv2lesson2/Main.java
@@ -7,21 +7,21 @@ public class Main {
     public static void main(String[] args) {
 
         String[][] arr = new String[][]{
-                {"1","a","3","4"} ,
+                {"1","2","8","4"} ,
                 {"5","6","7","8"},
                 {"9","10","11","12"},
                 {"13","14","15","16"}};
         try {
             try {
                 int result = method(arr);
-                System.out.println(result);
+                System.out.println("Сумма элементов массива =" + result);
             } catch (MyArraySizeException e) {
                 System.out.println("Размер массива превышен!");
             }
         }
         catch (MyArrayDataException e) {
             System.out.println("Неправильное значение массива!");
-            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j+ "- исправьте");
         }
 
     }
