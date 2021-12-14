974d9222f204cba964c73c0d0dfc5c815a375eca
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2021-12-14 23:02:26 +0300

    Initial commit

--- src/main/java/ru/geekbrains/jv2lesson2/Main.java
+++ src/main/java/ru/geekbrains/jv2lesson2/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         String[][] arr = new String[][]{
-                {"1","l","3","4"} ,
+                {"1","a","3","4"} ,
                 {"5","6","7","8"},
                 {"9","10","11","12"},
                 {"13","14","15","16"}};
