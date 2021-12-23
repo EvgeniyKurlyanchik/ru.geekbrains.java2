2d3b1cd63e4c5805d2dd6e8aa97f30c7949ca09d
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2021-12-24 00:22:17 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/jv2lesson3/ArrayWords.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.jv2lesson3;
+
+import java.util.ArrayList;
+
+public class ArrayWords {
+    public static void main(String[] args) {
+        ArrayList<String> numbers = new ArrayList<>();
+
+        numbers.add("Один");
+        numbers.add("Два");
+        numbers.add("Три");
+        numbers.add("Четыре");
+        numbers.add("Пять");
+        numbers.add("Четыре");
+        numbers.add("Три");
+        numbers.add("Два");
+        numbers.add("Один");
+        numbers.add("Два");
+        numbers.add("Три");
+        numbers.add("Один");
+        numbers.add("Два");
+        numbers.add("Один");
+        System.out.printf("Вся коллекция  составляет: %n" + numbers);
+    }
+
+
+
+
+}
